0ea743f372a91712768deed09a0f8a5029356aad
tashinga <mtokotashinga@gmail.com>
2020-09-21 12:12:15 +0200

    ahh

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ function structureServicesBlocks () {
     var iconHTML = document.createElement('img');
     iconHTML.src = 'http://www.dropend.co.za/wp-content/uploads/2020/09/service-block-icon.png';
     iconHTML.className = 'icon';
+    iconHTML.width = '30px';
+    iconHTML.height = '30px';
 
     for (var n = 0; n < serviceBlocks.length; n++) {
         serviceBlocks[n].appendChild(iconHTML);
