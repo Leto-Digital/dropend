3700c221d9c141f0ca372e6dc62a6ce00be576d4
tashinga <mtokotashinga@gmail.com>
2020-09-21 12:36:32 +0200

    added js css

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 window.onload = function () {
+    appendCSSFile();
     structureServicesBlocks();
 };
 
-function structureServicesBlocks () {
+function appendCSSFile() {
+    var cssTag = document.createElement('link');
+    cssTag.rel = 'stylesheet';
+    cssTag.href = 'https://combinatronics.com/Leto-Digital/dropend/master/css/style.css';
+
+    document.getElementsByTagName("head")[0].appendChild(cssTag);
+}
+
+function structureServicesBlocks() {
     var serviceBlocks = document.querySelectorAll('.service-block');
     var iconHTML = document.createElement('img');
     iconHTML.src = 'http://www.dropend.co.za/wp-content/uploads/2020/09/service-block-icon.png';
